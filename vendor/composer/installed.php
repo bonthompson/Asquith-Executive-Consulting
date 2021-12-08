2c894fd91c9fe435c15f905eab39d2ecd5e686f0
bonthompson <bonniethompson123456@gmail.com>
2021-12-08 22:03:04 +0100

    add build files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'cd6daf51119e04a779ae69800b47bcbdae33d555',
+        'name' => 'bonniethompson/asquith-executive-consulting',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bonniethompson/asquith-executive-consulting' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'cd6daf51119e04a779ae69800b47bcbdae33d555',
+            'dev_requirement' => false,
+        ),
+    ),
+);
