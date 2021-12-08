8ee32e22744076e865007051db0f5402e7865515
bonthompson <bonniethompson123456@gmail.com>
2021-12-08 22:36:10 +0100

    fixing build

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+// import App from './App'
 import reportWebVitals from './reportWebVitals'
+import HomePage from './pages/home-page/homepage'
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <HomePage />
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
